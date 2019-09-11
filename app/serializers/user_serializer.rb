e4e11af20a7987b6e40c6721ca1e7edf7d6ed3dc
github username <github email address>
2019-09-11 10:30:55 -0400

    added skills column to users

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :location
-  
+
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_09_193635) do
+ActiveRecord::Schema.define(version: 2019_09_11_142928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2019_09_09_193635) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "skills"
   end
 
   add_foreign_key "user_skills", "skills"
